c96db39be06e26f7d8b187c04fb8ec661a495a51
Indrajit Pal <indrajitpal389@gmail.com>
2024-07-22 16:06:59 +0545

    :wq qw i
    git
    dlfgljdoifjgcd "/home/indrajit/Desktop/apnacollege/Coding/" && javac CheckPrimeNumber.java && java CheckPrimeNumber
    cd "/home/indrajit/Desktop/apnacollege/Coding/" && javac CheckPrimeNumber.java && java CheckPrimeNumber
    cd "/home/indrajit/Desktop/apnacollege/Coding/" && javac CheckPrimeNumber.java && java CheckPrimeNumber
    cd "/home/indrajit/Desktop/apnacollege/Coding/" && javac CheckPrimeNumber.java && java CheckPrimeNumber

--- CheckPrimeNumber.java
+++ CheckPrimeNumber.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 
-/**
- * CheckPrimeNumber
- */
 public class CheckPrimeNumber {
+    public static boolean checkPrime(int n) {
+        boolean isprime = true;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isprime = false;
+                break;
+            }
+        }
+
+        return isprime;
+    }
 
     public static void main(String[] args) {
-        int arr[]=new int[20];
-        
-        
+        int n = 11;
+        if(checkPrime(n)){
+            System.out.println("prime number");
+        }else{
+            System.out.println("not a prime number");
+        }
 
     }
 }
